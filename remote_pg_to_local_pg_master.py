70793edb485c328769d69f7c45d1edeffd3d7e81
Pooh <71396594+punyapatkha@users.noreply.github.com>
2021-08-12 13:14:30 +0700

    [add] pg_to_pg

---
+++ remote_pg_to_local_pg_master.py
@@ -0,0 +1,49 @@
+#import lib
+
+import psycopg2
+import pandas as pd
+import logging
+import sqlalchemy
+
+#fuction declare
+
+
+#initial log
+logging.basicConfig(filename='pg_to_pg.log', filemode='a', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+#%%
+logging.info('open log')
+
+connection = psycopg2.connect(
+         host= '',
+         dbname= '',
+         user= 'postgres',
+         password= '',
+         port= '5432'
+     )
+connection.cursor()
+
+database_username = 'postgres'
+database_password = ''
+database_ip       = '127.0.0.1:5432'
+database_name     = ''
+
+database_connection = sqlalchemy.create_engine('postgresql+psycopg2://{0}:{1}@{2}/{3}'.
+                                                       format(database_username, database_password, 
+                                                              database_ip, database_name))
+
+#list of table name
+list_master_table = ['ADJUST_FACTOR','CALENDAR']
+for i in list_master_table:
+
+#for loop table names
+
+    SQL_Query = pd.read_sql_query(
+        '''SELECT * FROM "{i}" limit 10'''.format(i), connection)
+    x = pd.DataFrame(SQL_Query)
+    x = x.reset_index(drop=True)
+    x.to_sql(con=database_connection, name=i, if_exists='append',index=False
+        , method="multi", chunksize=1000)
+
+
+logging.info('close log')
+logging.shutdown()
